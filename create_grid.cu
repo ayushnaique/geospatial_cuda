6a75b5700ddfe9f38327e4423979029757c8f7e9
Aditya Vinod Kumar <ads.vinodk@gmail.com>
2024-11-04 19:56:56 -0500

    Minor edits: added coordinates of bounding box size

--- create_grid.cu
+++ create_grid.cu
@@ -7,6 +7,10 @@
 #include <vector>
 using namespace std;
 
+#define mp make_pair
+#define fi first
+#define se second
+
 struct Point {
 	int x, y;
 
@@ -14,16 +18,21 @@ struct Point {
 };
 
 struct Grid {
-	Point *bottom_left, *bottom_right, *top_left, *top_right;
+	Grid bottom_left, bottom_right, top_left, top_right;
+	Point *points;
 
 	// Initialize the corresponding Point values
-	Grid(Point *bl, Point *br, Point *tl, Point *tr)
-		: bottom_left(bl), bottom_right(br), top_left(tl), top_right(tr) {}
+	Grid(Grid bl, Grid br, Grid tl, Grid tr, Point *ps)
+		: bottom_left(bl),
+		  bottom_right(br),
+		  top_left(tl),
+		  top_right(tr),
+		  points(ps) {}
 };
 
 __global__ void categorize_points(Point *d_points, int *d_categories,
 								  int *grid_counts, int count, int range,
-								  int middle) {
+								  int middle_x, int middle_y) {
 	// subgrid_counts declared outside kernel, Dynamic Shared Memory
 	// Accessed using extern
 	extern __shared__ int subgrid_counts[];
@@ -43,22 +52,22 @@ __global__ void categorize_points(Point *d_points, int *d_categories,
 	for (int i = start; i < start + range; i++) {
 		if (i < count) {
 			// bottom left; if the point lies in bottom left, increment
-			if (d_points[i].x <= middle and d_points[i].y <= middle) {
+			if (d_points[i].x <= middle_x and d_points[i].y <= middle_y) {
 				d_categories[i] = 1;
 				first++;
 			}
 			// bottom right; if point lies in bottom right, increment
-			else if (d_points[i].x > middle and d_points[i].y <= middle) {
+			else if (d_points[i].x > middle_x and d_points[i].y <= middle_y) {
 				d_categories[i] = 2;
 				second++;
 			}
 			// top left; if point lies in top left, increment
-			else if (d_points[i].x <= middle and d_points[i].y > middle) {
+			else if (d_points[i].x <= middle_x and d_points[i].y > middle_y) {
 				d_categories[i] = 3;
 				third++;
 			}
 			// top right; if point lies in top right, increment
-			else if (d_points[i].x > middle and d_points[i].y > middle) {
+			else if (d_points[i].x > middle_x and d_points[i].y > middle_y) {
 				d_categories[i] = 4;
 				fourth++;
 			}
@@ -122,7 +131,9 @@ __global__ void organize_points(Point *d_points, int *d_categories, Point *bl,
 	}
 }
 
-void quadtree_grid(vector<Point> points, int count, int dimension) {
+void quadtree_grid(vector<Point> points, int count,
+				   pair<int, int> bottom_left_corner,
+				   pair<int, int> top_right_corner) {
 	// Array of points for the geospatial data
 	Point *d_points;
 
@@ -156,15 +167,18 @@ void quadtree_grid(vector<Point> points, int count, int dimension) {
 			static_cast<float>(count) / (num_blocks * threads_per_block);
 		range = std::ceil(value);
 	}
-	printf("GPU: %d blocks of %d threads each with range=%d\n", num_blocks,
-		   threads_per_block, range);
+	printf("Categorize in GPU: %d blocks of %d threads each with range=%d\n",
+		   num_blocks, threads_per_block, range);
 
 	dim3 grid(num_blocks, 1, 1);
 	dim3 block(threads_per_block, 1, 1);
 
 	// KERNEL Function to categorize points into 4 subgrids
+	int middle_x = (top_right_corner.fi - bottom_left_corner.fi) / 2,
+		middle_y = (top_right_corner.se - bottom_left_corner.se) / 2;
 	categorize_points<<<grid, block, 4 * sizeof(int)>>>(
-		d_points, d_categories, d_grid_counts, count, range, dimension / 2);
+		d_points, d_categories, d_grid_counts, count, range, middle_x,
+		middle_y);
 
 	// Get back the data from device to host
 	cudaMemcpy(h_categories.data(), d_categories, count * sizeof(int),
@@ -172,19 +186,16 @@ void quadtree_grid(vector<Point> points, int count, int dimension) {
 	cudaMemcpy(h_grid_counts.data(), d_grid_counts, 4 * sizeof(int),
 			   cudaMemcpyDeviceToHost);
 
-	// for(int i = 0; i<1000; i++){
-	// printf("x = %d, y = %d, category = %d\n", points[i].x, points[i].y,
-	// h_categories[i]);
-	//}
-	// int total = 0;
-	// for(int i = 0; i<4; i++){
-	// printf("sub grid %d - %d\n", i+1, h_grid_counts[i]);
-	// total += h_grid_counts[i];
-	//}
-	// printf("Total Count - %d\n", count);
-	// if(total == count){
-	// printf("Matches\n");
-	//}
+	int total = 0;
+	printf("Point counts per sub grid - \n");
+	for (int i = 0; i < 4; i++) {
+		printf("sub grid %d - %d\n", i + 1, h_grid_counts[i]);
+		total += h_grid_counts[i];
+	}
+	printf("Total Count - %d\n", count);
+	if (total == count) {
+		printf("Sum of sub grid counts matches total point count\n\n");
+	}
 
 	// Declare arrays for each section of the grid and allocate memory depending
 	// on the number of points found
@@ -198,6 +209,10 @@ void quadtree_grid(vector<Point> points, int count, int dimension) {
 	dim3 block2(threads_per_block, 1, 1);
 
 	// KERNEL Function to assign the points to its respective array
+	float value = static_cast<float>(count) / threads_per_block;
+	range = std::ceil(value);
+	printf("Organize in GPU: 1 block of %d threads each with range=%d\n",
+		   threads_per_block, range);
 	organize_points<<<grid2, block2, 4 * sizeof(int)>>>(
 		d_points, d_categories, bottom_left, bottom_right, top_left, top_right,
 		count, count / threads_per_block);
@@ -220,10 +235,14 @@ void quadtree_grid(vector<Point> points, int count, int dimension) {
 	cudaMemcpy(tr, top_right, h_grid_counts[3] * sizeof(Point),
 			   cudaMemcpyDeviceToHost);
 
-	printf("Point in bottom left - %d %d\n", bl[0].x, bl[0].y);
-	printf("Point in bottom right - %d %d\n", br[0].x, br[0].y);
-	printf("Point in top left - %d %d\n", tl[0].x, tl[0].y);
-	printf("Point in top right - %d %d\n", tr[0].x, tr[0].y);
+	printf("Sample Point in bottom left - %d %d\n", bl[0].x, bl[0].y);
+	printf("Sample Point in bottom right - %d %d\n", br[0].x, br[0].y);
+	printf("Sample Point in top left - %d %d\n", tl[0].x, tl[0].y);
+	printf("Sample Point in top right - %d %d\n", tr[0].x, tr[0].y);
+
+	// TODO:
+	// Recursively call the quadtree grid function on each of the 4 sub grids -
+	// bl, br, tl, tr and store in Grid struct
 
 	// Free data
 	cudaFree(d_points);
@@ -262,7 +281,7 @@ int main() {
 
 	file.close();
 
-	quadtree_grid(points, point_count, 1000);
+	quadtree_grid(points, point_count, mp(0, 0), mp(1000, 1000));
 
 	return 0;
 }
