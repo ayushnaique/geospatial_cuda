4b07f58e965fe624263306309fe8dd42a87d84a4
Aditya Vinod Kumar <ads.vinodk@gmail.com>
2024-11-13 01:26:46 -0500

    Feature: dynamic grid dimensions and updated sequential code

--- generate_points.py
+++ generate_points.py
@@ -1,15 +1,16 @@
+from tqdm import tqdm
 import random
 
 def generate_random_points(filename, num_points, grid_size=(1e6, 1e6)):
     with open(filename, "w") as file:
-        for _ in range(int(num_points)):
+        for _ in tqdm(range(int(num_points))):
             x = random.randint(0, grid_size[0] - 1)
             y = random.randint(0, grid_size[1] - 1)
             file.write(f"{x} {y}\n")
 
 def main():
     # Approximate maximum points to keep file size under 1 GB
-    max_points = 1e5
+    max_points = 1e6
     filename = "points.txt"
     
     generate_random_points(filename, max_points)
